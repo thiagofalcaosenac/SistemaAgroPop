a11be4f77f89864b88951913f8849fb5d81c4326
thiagofalcaosenac <thiago.falcao@alunos.sc.senac.br>
2023-05-31 21:57:12 -0300

    continuacao criação endereco

--- Model/Endereco.cs
+++ Model/Endereco.cs
@@ -11,11 +11,25 @@ namespace Model
         public int id { get; set; }
         public string telefone { get; set; }
         public string email { get; set; }
+        public string bairro { get; set; }
+        public string rua { get; set; }
+        public string numero { get; set; }
+        public string complemento { get; set; }        
+        public string cidade { get; set; }
+        public string estado { get; set; }
 
-        public Endereco(string telefone, string email)
+        public Endereco(string telefone, string email, string bairro, string rua, string numero, 
+                            string complemento, string cidade, string estado)
         {
             this.telefone = telefone;
             this.email = email;
+            this.bairro = bairro;
+            this.rua = rua;
+            this.numero = numero;
+            this.complemento = complemento;
+            this.cidade = cidade;
+            this.estado = estado;
+            
             Database db = new Database();
             db.Enderecos.Add(this);
             db.SaveChanges();
