80b42fb443525ef64290565743ecc49591699c11
thiagofalcaosenac <thiago.falcao@alunos.sc.senac.br>
2023-06-01 20:56:38 -0300

    Criação do Model Fazenda

---
+++ Migrations/20230601235533_CriarModelFazenda.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaAgroPop.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarModelFazenda : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fazendas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    qtdLimiteAnimal = table.Column<int>(type: "int", nullable: false),
+                    idEndereco = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fazendas", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fazendas");
+        }
+    }
+}
---
+++ Model/Fazenda.cs
@@ -0,0 +1,146 @@
+using Model;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+
+namespace Model
+{
+    public class Fazenda
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int qtdLimiteAnimal { get; set; }
+        //public virtual int qtdAtualAnimal { get; set; }
+        public int idEndereco { get; set; }
+
+        public Fazenda(string nome, int qtdLimiteAnimal, int idEndereco)
+        {
+            this.nome = nome;
+            this.qtdLimiteAnimal = qtdLimiteAnimal;
+            this.idEndereco = idEndereco;
+
+            Database db = new Database();
+            db.Fazendas.Add(this);
+            db.SaveChanges();
+        }
+
+        public Fazenda()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {id}, Nome: {nome}, Quantidade Limite Animal: {qtdLimiteAnimal}, Id Endereco: {idEndereco}";
+        }
+
+        public override int GetHashCode()
+        {
+            return id.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj == this)
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            Fazenda fazenda = (Fazenda)obj;
+            return this.id == fazenda.id;
+        }
+
+        public static Model.Fazenda BuscarFazenda(
+            int id
+        )
+        {
+            Database db = new Database();
+            try
+            {
+                Model.Fazenda fazenda = (from u in db.Fazendas
+                                          where u.id == id
+                                          select u).First();
+                return fazenda;
+            }
+            catch
+            {
+                throw new System.Exception("Fazenda não encontrado");
+            }
+        }
+
+        public static Fazenda AlterarFazenda(
+            int id,
+            string nome,
+            int qtdLimiteAnimal,
+            int idEndereco
+        )
+        {
+            try
+            {
+                Fazenda fazenda = BuscarFazenda(id);
+                fazenda.nome = nome;
+                fazenda.qtdLimiteAnimal = qtdLimiteAnimal;
+                fazenda.idEndereco = idEndereco;
+
+                Database db = new Database();
+                db.Fazendas.Update(fazenda);
+                db.SaveChanges();
+
+                return fazenda;
+            }
+            catch (System.Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static void ExcluirFazenda(
+            int id
+        )
+        {
+            try
+            {
+                Model.Fazenda fazenda = BuscarFazenda(id);
+                Database db = new Database();
+                db.Fazendas.Remove(fazenda);
+                db.SaveChanges();
+            }
+            catch (System.Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static List<Model.Fazenda> ListarFazendas()
+        {
+            Database db = new Database();
+            List<Model.Fazenda> fazendas = (from u in db.Fazendas
+                                             select u).ToList();
+            return fazendas;
+        }
+
+        public static Model.Fazenda BuscarPorNome(string nome)
+        {
+            try
+            {
+                Database db = new Database();
+                Model.Fazenda fazenda = (from u in db.Fazendas
+                                          where u.nome == nome
+                                          select u).First();
+                return fazenda;
+            }
+            catch
+            {
+                throw new System.Exception("Fazenda não encontrado");
+            }
+        }
+    }
+
+}
