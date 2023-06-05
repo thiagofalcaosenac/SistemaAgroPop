837eb452fe52b4eab59ee7bf48415bf2b6148730
AlessandraFranco23 <alessandrafranco2311@gmail.com>
2023-06-04 22:22:09 -0300

    Criação do Model CarteiraVacinacao

---
+++ Migrations/20230605012107_CarteiraVacinacao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaAgroPop.Migrations
+{
+    /// <inheritdoc />
+    public partial class CarteiraVacinacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarteiraVacinacoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DataVacinacao = table.Column<DateOnly>(type: "date", nullable: false),
+                    ProximaDose = table.Column<DateOnly>(type: "date", nullable: false),
+                    NroDose = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarteiraVacinacoes", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarteiraVacinacoes");
+        }
+    }
+}
